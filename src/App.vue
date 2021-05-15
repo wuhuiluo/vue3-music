f0d1cb132a6e4e93d089bb41acc862fdd0914a44
wuhuiluo <510426343@qq.com>
2021-05-15 21:54:00 +0800

    latest commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <m-header />
+  <tab />
 </template>
 <script>
-import MHeader from '@/components/header/header'
-
+import MHeader from "@/components/header/header";
+import Tab from "@/components/tab/tab";
 export default {
   components: {
-    MHeader
-  }
-}
+    MHeader,
+    Tab,
+  },
+};
 </script>
 <style lang="scss">
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,29 @@ import {
   createRouter,
   createWebHashHistory
 } from 'vue-router'
+import Recommend from '@/views/recommend.vue'
+import Singer from '@/views/singer.vue'
+import Search from '@/views/search.vue'
+import TopList from '@/views/top-list.vue'
 
-const routes = [
+
+const routes = [{
+    path: '/',
+    redirect: '/recommend'
+  }, {
+    path: '/recommend',
+    component: Recommend
+  },
+  {
+    path: '/singer',
+    component: Singer
+  }, {
+    path: '/search',
+    component: Search
+  }, {
+    path: '/top-list',
+    component: TopList
+  }
 
 ]
 
